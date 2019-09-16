e92953674a21bcdb74174af684a6bda32c399b10
Christopher Hernandez <cerberean661@gmail.com>
2019-09-16 16:02:02 -0700

    verifying disabled button and click button status

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import Controls from './Controls';
 
 afterEach(cleanup);
@@ -10,12 +10,22 @@ describe('<Controls />', () => {
     });
 
     it('open and unlocked', () => {
-        const { getByText } = render(<Controls closed={false} locked={false} />);
+        const closeSpy = jest.fn();
+        const lockSpy = jest.fn();
+        const { getByText } = render(<Controls closed={false}
+                                                locked={false}
+                                                toggleClosed={closeSpy}
+                                                toggleLocked={lockSpy}
+                                                />);
         const closeButton = getByText(/close gate/i);
         const lockButton = getByText(/lock gate/i);
 
-        
+        // verifying disabled button
         expect(closeButton.disabled).toBeFalsy();
         expect(lockButton.disabled).toBeTruthy();
+
+        // verifying button click status
+        fireEvent.click(closeButton);
+        expect(closeSpy).toBeCalled();
     });
 });
