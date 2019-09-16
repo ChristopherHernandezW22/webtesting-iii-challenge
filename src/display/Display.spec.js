21a3cf5826f6e9bdb849b7b157975709bf5db7e6
Christopher Hernandez <cerberean661@gmail.com>
2019-09-16 14:20:48 -0700

    running tests on Display.spec.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,7 +1,7 @@
 // Test away!
 import React from 'react';
 import Display from './Display';
-import { render } from '@testing-library/react';
+import { render, queryByText } from '@testing-library/react';
 
 describe('<Display />', () => {
     it('render without crash', () => {
@@ -10,7 +10,12 @@ describe('<Display />', () => {
 
     it('open/unlocked', () => {
         const { getByText } = render(<Display closed={false} locked={false} />);
+        
+        // checking for correct text
         getByText(/unlocked/i);
         getByText(/open/i);
+
+        // making sure incorrect text does not show up in the document
+        expect(queryByText(/closed/i)).toBe(null);
     });
 });
