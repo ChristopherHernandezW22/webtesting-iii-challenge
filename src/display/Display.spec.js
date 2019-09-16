33ba31be0f3c476fad4bc0416b194b473738fd8f
Christopher Hernandez <cerberean661@gmail.com>
2019-09-16 09:56:48 -0700

    running first test in Display.spec.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import Display from './Display';
+import { render } from '@testing-library/react';
+
+describe('<Display />', () => {
+    it('render without crash', () => {
+        render(<Display />);
+    });
+
+    it('open/unlocked', () => {
+        const { getByText } = render(<Display closed={false} locked={false} />);
+        getByText(/unlocked/i);
+        getByText(/open/i);
+    });
+});
