66e0833171517068f62e9988fa7b574e4eaa2fbd
Christopher Hernandez <cerberean661@gmail.com>
2019-09-16 17:24:50 -0700

    tests passed for Controls.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -27,5 +27,56 @@ describe('<Controls />', () => {
         // verifying button click status
         fireEvent.click(closeButton);
         expect(closeSpy).toBeCalled();
+
+        fireEvent.click(lockButton);
+        expect(lockSpy).not.toBeCalled();
+    });
+
+
+    it('closed and unlocked', () => {
+        const closeSpy = jest.fn();
+        const lockSpy = jest.fn();
+        const { getByText } = render(<Controls closed={true}
+                                                locked={false}
+                                                toggleClosed={closeSpy}
+                                                toggleLocked={lockSpy}
+                                                />);
+        const closeButton = getByText(/open gate/i);
+        const lockButton = getByText(/lock gate/i);
+
+        // verifying disabled button
+        expect(closeButton.disabled).toBeFalsy();
+        expect(lockButton.disabled).toBeFalsy();
+
+        // verifying button click status
+        fireEvent.click(closeButton);
+        expect(closeSpy).toBeCalled();
+
+        fireEvent.click(lockButton);
+        expect(lockSpy).toBeCalled();
+    });
+
+
+    it('closed and locked', () => {
+        const closeSpy = jest.fn();
+        const lockSpy = jest.fn();
+        const { getByText } = render(<Controls closed={true}
+                                                locked={true}
+                                                toggleClosed={closeSpy}
+                                                toggleLocked={lockSpy}
+                                                />);
+        const closeButton = getByText(/open gate/i);
+        const lockButton = getByText(/unlock gate/i);
+
+        // verifying disabled button
+        expect(closeButton.disabled).toBeTruthy();
+        expect(lockButton.disabled).toBeFalsy();
+
+        // verifying button click status
+        fireEvent.click(closeButton);
+        expect(closeSpy).not.toBeCalled();
+
+        fireEvent.click(lockButton);
+        expect(lockSpy).toBeCalled();
     });
 });
